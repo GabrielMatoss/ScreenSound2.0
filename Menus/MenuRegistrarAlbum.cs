46aa8c133bfab2e95a9118f6f648d3ca1d285813
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-03-28 16:38:33 -0300

    (feat): adds code for using classes that register an album and display its details.

--- Menus/MenuRegistrarAlbum.cs
+++ Menus/MenuRegistrarAlbum.cs
@@ -11,13 +11,13 @@ internal class MenuRegistrarAlbum : Menu
         ExibirTituloDaOpcao("Registro de álbuns");
         Console.Write("Digite a banda cujo álbum deseja registrar: ");
         string nomeDaBanda = Console.ReadLine()!;
-        if(bandasRegistradas.ContainsKey(nomeDaBanda))
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
         {
         Console.Write("Agora digite o título do álbum: ");
         string tituloAlbum = Console.ReadLine()!;
         Banda banda = bandasRegistradas[nomeDaBanda];
         banda.AdicionarAlbum(new Album(tituloAlbum));
-        Console.WriteLine($"O álbum {tituloAlbum} de {nomeDaBanda} foi registrado com sucesso!");
+        Console.WriteLine($"O álbum {tituloAlbum} da banda {nomeDaBanda} foi registrado com sucesso!");
         Thread.Sleep(4000);
         Console.Clear();
         }
